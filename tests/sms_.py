32df9c919ba33ffd862ee4cec5707eaad5d798ed
LuckElixir <sriniranjan.gs@gmail.com>
2025-05-31 02:10:11 -0400

    Add SMS feature

--- tests/sms_.py
+++ tests/sms_.py
@@ -1,20 +1,8 @@
+import smtplib, ssl
+import traceback
 
-# REQUIRES AIOSMTPLIB thats why theres 1 error with the code
-# pip install aiosmtplib!!!! 
-
-import asyncio
-import re
-from email.message import EmailMessage
-from typing import Collection, List, Tuple, Union
-import aiosmtplib
-
-HOST = "smtp.gmail.com"
-# https://kb.sandisk.com/app/answers/detail/a_id/17056/~/list-of-mobile-carrier-gateway-addresses
-# https://www.gmass.co/blog/send-text-from-gmail/
-
-# https://www.fbbbrown.com/help-faq/app-help/runsafe/list-of-mobile-carrier-gateway-addresses/ USE THIS INSTEAD OF THE FIRST LINK
-
-CARRIER_MAP = {
+def send_sms(sender):
+    CARRIER_MAP = {
     "verizon": "vtext.com",
     "tmobile": "tmomail.net",
     "sprint": "messaging.sprintpcs.com",
@@ -23,44 +11,27 @@ CARRIER_MAP = {
     "cricket": "sms.cricketwireless.net",
     "uscellular": "email.uscc.net",
 }
+    port = 465  # For TLS
+    smtp_server = "smtp.gmail.com"
+    sender_email = "noreply.skipthequeue@gmail.com"  # Enter your address
+    receiver_email = sender  # Enter receiver address
+    password = "bmyp tpwf uwdk uufn"
+    message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+    context = ssl.create_default_context()
+    try:
+        for value in CARRIER_MAP.values():
+            try:
+                with smtplib.SMTP_SSL(smtp_server, port) as server:
+                    server.login(sender_email, password)
+                    server.sendmail(sender_email, receiver_email, message)
+            except Exception as e:
+                continue
+        print("Text sent successfully!")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print(traceback.format_exc())
 
 
-# pylint: disable=too-many-arguments
-async def send_txt(
-    num: Union[str, int], carrier: str, email: str, pword: str, msg: str, subj: str
-) -> Tuple[dict, str]:
-    to_email = CARRIER_MAP[carrier]
-
-    # build message
-    message = EmailMessage()
-    message["From"] = email
-    message["To"] = f"{num}@{to_email}"
-    message["Subject"] = subj
-    message.set_content(msg)
-
-    # send
-    send_kws = dict(username=email, password=pword, hostname=HOST, port=587, start_tls=True)
-    res = await aiosmtplib.send(message, **send_kws)  # type: ignore
-    msg = "failed" if not re.search(r"\sOK\s", res[1]) else "succeeded"
-    print(msg)
-    return res
-
-
-async def send_txts(
-    nums: Collection[Union[str, int]], carrier: str, email: str, pword: str, msg: str, subj: str
-) -> List[Tuple[dict, str]]:
-    tasks = [send_txt(n, carrier, email, pword, msg, subj) for n in set(nums)]
-    return await asyncio.gather(*tasks)
-
-
-if __name__ == "__main__":
-    _num = "999999999" # should be pulled from data base-- gateway carrier is already set cause of code below 
-    _carrier = "tmobile" # ummmmmm idk how we can do every carrier-- we can just use anybody with t mobile
-    _email = "noreply.skipthequeue@gmail.com"
-    _pword = "bmyp tpwf uwdk uufn" 
-    _msg = "Hello, you are now ready!" # we have to find a way to update queue based on the Plus and Minus front end page
-    _subj = "Dummy subj" # name of the guy who we are sending the text to-- needs to be pulled from database
-    coro = send_txt(_num, _carrier, _email, _pword, _msg, _subj)
-    # _nums = {"999999999", "000000000"}
-    # coro = send_txts(_nums, _carrier, _email, _pword, _msg, _subj)
-    asyncio.run(coro)
