8c7729dc1283de2988912d2b08f4901c98147f09
polarems <mailboxofyg@gmail.com>
2025-05-31 01:58:22 -0400

    SMSChanges

---
+++ tests/sms_.py
@@ -0,0 +1,66 @@
+
+# REQUIRES AIOSMTPLIB thats why theres 1 error with the code
+# pip install aiosmtplib!!!! 
+
+import asyncio
+import re
+from email.message import EmailMessage
+from typing import Collection, List, Tuple, Union
+import aiosmtplib
+
+HOST = "smtp.gmail.com"
+# https://kb.sandisk.com/app/answers/detail/a_id/17056/~/list-of-mobile-carrier-gateway-addresses
+# https://www.gmass.co/blog/send-text-from-gmail/
+
+# https://www.fbbbrown.com/help-faq/app-help/runsafe/list-of-mobile-carrier-gateway-addresses/ USE THIS INSTEAD OF THE FIRST LINK
+
+CARRIER_MAP = {
+    "verizon": "vtext.com",
+    "tmobile": "tmomail.net",
+    "sprint": "messaging.sprintpcs.com",
+    "at&t": "txt.att.net",
+    "boost": "smsmyboostmobile.com",
+    "cricket": "sms.cricketwireless.net",
+    "uscellular": "email.uscc.net",
+}
+
+
+# pylint: disable=too-many-arguments
+async def send_txt(
+    num: Union[str, int], carrier: str, email: str, pword: str, msg: str, subj: str
+) -> Tuple[dict, str]:
+    to_email = CARRIER_MAP[carrier]
+
+    # build message
+    message = EmailMessage()
+    message["From"] = email
+    message["To"] = f"{num}@{to_email}"
+    message["Subject"] = subj
+    message.set_content(msg)
+
+    # send
+    send_kws = dict(username=email, password=pword, hostname=HOST, port=587, start_tls=True)
+    res = await aiosmtplib.send(message, **send_kws)  # type: ignore
+    msg = "failed" if not re.search(r"\sOK\s", res[1]) else "succeeded"
+    print(msg)
+    return res
+
+
+async def send_txts(
+    nums: Collection[Union[str, int]], carrier: str, email: str, pword: str, msg: str, subj: str
+) -> List[Tuple[dict, str]]:
+    tasks = [send_txt(n, carrier, email, pword, msg, subj) for n in set(nums)]
+    return await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    _num = "999999999" # should be pulled from data base-- gateway carrier is already set cause of code below 
+    _carrier = "tmobile" # ummmmmm idk how we can do every carrier-- we can just use anybody with t mobile
+    _email = "noreply.skipthequeue@gmail.com"
+    _pword = "bmyp tpwf uwdk uufn" 
+    _msg = "Hello, you are now ready!" # we have to find a way to update queue based on the Plus and Minus front end page
+    _subj = "Dummy subj" # name of the guy who we are sending the text to-- needs to be pulled from database
+    coro = send_txt(_num, _carrier, _email, _pword, _msg, _subj)
+    # _nums = {"999999999", "000000000"}
+    # coro = send_txts(_nums, _carrier, _email, _pword, _msg, _subj)
+    asyncio.run(coro)
